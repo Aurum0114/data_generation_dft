2dea927bdd4276d56d2b01f1507380899857d037
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-03-18 15:32:33 +0100

    new design allows for sampling from only one flavour + sampling is now a separate function

--- create_placeholder_categories.py
+++ create_placeholder_categories.py
@@ -1,20 +1,18 @@
 import argparse
 import json
-import math
 import os
 import random
 import shutil
-import numpy as np
 
 import utils.xyz_utils as xyz
 
-def create_placeholder_categories(flavour_file, molecule_xyz_file, num_molecules, output_temp_dir):
+def create_placeholder_categories(flavour_file, molecule_xyz_file, output_temp_dir, num_molecules, flavour_num=None):
     """
     This function creates placeholder categories that will be used by the next function to create the actual categories.
-    :param flavour_file: Stores two lists: all functionals and basis sets. Example file: example_files/func_and_base.json
-    :param molecule_xyz_file: Filtered dataset to sample molecules from. Example file: example_files/inputs.xyz
-    :param num_molecules: The number of molecules per category
-    :param output_temp_dir: The directory that stores the temporary placeholder categories
+    :flavour_file: Stores two lists: all functionals and basis sets. Example file: example_files/func_and_base.json
+    :molecule_xyz_file: Filtered dataset to sample molecules from. Example file: example_files/inputs.xyz
+    :output_temp_dir: The directory that stores the temporary placeholder categories
+    :num_molecules: The number of molecules per datatype per category
     """
     if os.path.exists(output_temp_dir):
         shutil.rmtree(output_temp_dir)
@@ -34,7 +32,6 @@ def create_placeholder_categories(flavour_file, molecule_xyz_file, num_molecules
         for b in basissets:
             t = {
                 "number": str(index),
-                "set": None,  # train or test
                 "functional": f,
                 "basisset": b,
                 "num_molecules": num_molecules
@@ -45,27 +42,37 @@ def create_placeholder_categories(flavour_file, molecule_xyz_file, num_molecules
     num_flavours = len(dft_flavours)
     print("Found number of flavours is: ", num_flavours)
 
-    num_digits_needed = math.ceil(np.log10(num_flavours))
-    for single_flavour in dft_flavours:
-        # sample X molecules
-        coords_all, elements_all = xyz.readXYZs(molecule_xyz_file)
-        assert len(coords_all) == len(elements_all)
-        num_all_mol = len(coords_all)
-        sampled_indices = random.sample(range(num_all_mol), int(single_flavour["num_molecules"]))
-        coords = [coords_all[i] for i in sampled_indices]
-        elements = [elements_all[i] for i in sampled_indices]
+    coords_all, elements_all = xyz.readXYZs(molecule_xyz_file)
+    assert len(coords_all) == len(elements_all)
+    num_all_mol = len(coords_all)
 
-        task_coord_filename = f"data_01_{single_flavour['functional']}###{single_flavour['basisset']}.xyz"
-        task_dir_path = os.path.join(temp_task_dir,
-                                     f"T_{str(single_flavour['number']).zfill(num_digits_needed)}_"
-                                     f"{single_flavour['functional']}###{single_flavour['basisset']}")
+    if flavour_num==None:
+        for single_flavour in dft_flavours:
+            sample_molecules_for_flavour(single_flavour, dft_flavours, coords_all, elements_all, temp_task_dir)
 
-        os.mkdir(task_dir_path)
+    elif str(flavour_num).isnumeric() and 1 <= int(flavour_num) < len(dft_flavours):
+        single_flavour = dft_flavours[flavour_num]
+        sample_molecules_for_flavour(single_flavour, dft_flavours, coords_all, elements_all, temp_task_dir)
+        
+    else:
+        raise ValueError(f'Flavour value should be an integer (index of the flavour to use), maximum value is {dft_flavours[-1]['number']}')
+    
 
-        xyz.exportXYZs(coords, elements, os.path.join(task_dir_path, task_coord_filename))
+def sample_molecules_for_flavour(single_flavour, dft_flavours, coords_all, elements_all, temp_task_dir):
+    num_all_mol = len(coords_all)
+    sampled_indices = random.sample(range(num_all_mol), int(single_flavour["num_molecules"]))
+    coords = [coords_all[i] for i in sampled_indices]
+    elements = [elements_all[i] for i in sampled_indices]
 
-        with open(os.path.join(task_dir_path, "info.json"), 'w') as fp:
-            json.dump(single_flavour, fp)
+    task_coord_filename = f"data_01_{single_flavour['functional']}###{single_flavour['basisset']}.xyz"
+    task_dir_path = os.path.join(temp_task_dir,
+                                f"flavour{dft_flavours['number']}_{single_flavour['functional']}###{single_flavour['basisset']}")
+
+    os.mkdir(task_dir_path)
+    xyz.exportXYZs(coords, elements, os.path.join(task_dir_path, task_coord_filename))
+
+    with open(os.path.join(task_dir_path, "task_info.json"), 'w') as fp:
+        json.dump(single_flavour, fp)
 
 
 if __name__ == "__main__":
