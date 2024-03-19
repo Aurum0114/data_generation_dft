047ec10929986ff872d96456fd5cc03fbc2e8f9a
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-03-19 09:14:15 +0100

    more planning comments

--- create_placeholder_categories.py
+++ create_placeholder_categories.py
@@ -6,11 +6,11 @@ import shutil
 
 import utils.xyz_utils as xyz
 
-def create_placeholder_categories(flavour_file, molecule_xyz_file, output_temp_dir, num_molecules, flavour_idx=None):
+def create_placeholder_categories(flavour_file, xyz_files_dir, output_temp_dir, num_molecules, flavour_idx=None):
     """
     This function creates placeholder categories that will be used by the next function to create the actual categories.
     :flavour_file: Stores two lists: all functionals and basis sets. Example file: example_files/func_and_base.json
-    :molecule_xyz_file: Filtered dataset to sample molecules from. Example file: example_files/inputs.xyz
+    :xyz_files_dir: Filtered dataset to sample molecules from. Example file: example_files/inputs.xyz
     :output_temp_dir: The directory that stores the temporary placeholder categories
     :num_molecules: The number of molecules per datatype per category
     """
@@ -42,26 +42,39 @@ def create_placeholder_categories(flavour_file, molecule_xyz_file, output_temp_d
     num_flavours = len(dft_flavours)
     print("Found number of flavours is: ", num_flavours)
 
-    coords_all, elements_all = xyz.readXYZs(molecule_xyz_file)
-    assert len(coords_all) == len(elements_all)
+
+    datatypes = find_all_subdirs(xyz_files_dir)
+    #coords_all, elements_all = xyz.readXYZs(molecule_xyz_file)
+    #assert len(coords_all) == len(elements_all)
 
     if flavour_idx==None:
         for single_flavour in dft_flavours:
-            sample_molecules_for_flavour(single_flavour, coords_all, elements_all, temp_task_dir)
+            sample_molecules_for_flavour(single_flavour, datatypes, temp_task_dir)
 
     elif str(flavour_idx).isnumeric() and 1 <= int(flavour_idx) <= len(dft_flavours):
         single_flavour = dft_flavours[flavour_idx-1]
-        sample_molecules_for_flavour(single_flavour, coords_all, elements_all, temp_task_dir)
+        sample_molecules_for_flavour(single_flavour, datatypes, temp_task_dir)
         
     else:
         raise ValueError(f"Flavour value should be an integer (index of the flavour to use), maximum value is {dft_flavours[-1]['number']}")
     
+def find_all_subdirs(xyz_files_dir):
+    all_items = os.listdir(xyz_files_dir)
+    subdirectories = [item for item in all_items if os.path.isdir(os.path.join(xyz_files_dir, item))]
+    return subdirectories
 
-def sample_molecules_for_flavour(single_flavour, coords_all, elements_all, temp_task_dir):
-    num_all_mol = len(coords_all)
-    sampled_indices = random.sample(range(num_all_mol), int(single_flavour["num_molecules"]))
-    coords = [coords_all[i] for i in sampled_indices]
-    elements = [elements_all[i] for i in sampled_indices]
+def sample_molecules_for_flavour(single_flavour, datatypes, temp_task_dir):
+    #num_all_mol = len(coords_all)
+    datapoints_to_sample = int(single_flavour["num_molecules"])
+    coords = []
+    elements = []
+
+    for datatype in datatypes:
+        sample_molecules_for_datatype(coords, elements, datapoints_to_sample)
+
+    #sampled_indices = random.sample(range(num_all_mol), datapoints_to_sample)
+    #coords = [coords_all[i] for i in sampled_indices]
+    #elements = [elements_all[i] for i in sampled_indices]
 
     task_coord_filename = f"data_01_{single_flavour['functional']}###{single_flavour['basisset']}.xyz"
     task_dir_path = os.path.join(temp_task_dir,
@@ -74,9 +87,20 @@ def sample_molecules_for_flavour(single_flavour, coords_all, elements_all, temp_
     with open(os.path.join(task_dir_path, "task_info.json"), 'w') as fp:
         json.dump(single_flavour, fp)
 
-def sample_for_datatype():
-    pass
+def sample_molecules_for_datatype(coords, elements, datatype, datapoints_to_sample):
+    #go into that directory
+    #find the coords file
+    #find the info file
 
+    #read out the coords file
+    #change reading function so that it adds the charge
+    #coords_all, elements_all = xyz.readXYZs(molecule_xyz_file)
+    
+    #find number of molecules
+    #sampled_indices = random.sample(range(num_all_mol), datapoints_to_sample)
+    #list compreh for coords and elements
+    #append to the total coords and elements lists
+    pass
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
