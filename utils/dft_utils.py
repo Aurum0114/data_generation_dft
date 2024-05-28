a29d6801c1831577869967a6e6f212e0386e47b1
ahryncyszyn <ahryncyszyn11@gmail.com>
2024-05-28 10:15:31 +0200

    changed return 0 to return empy list

--- utils/dft_utils.py
+++ utils/dft_utils.py
@@ -350,7 +350,7 @@ def read_dft_grad():
             grad.append([float(line.split()[0]), float(line.split()[1]), float(line.split()[2])])
     
     if len(grad) == 0:
-        return 0
+        return []
     else:
         grad = np.array(grad) * HToeV * AToBohr
         grad = grad * (-1.0)
